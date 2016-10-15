5f729806b5f687ae319ef723a4f185d0433619c3
Abraham Quintero <abrahamq@mit.edu>
2016-10-14 20:56:47 -0400

    extremely unsafe exec

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express'); 
 var path = require('path'); 
+var shell = require('shelljs'); 
 
 //var index = require('./index'); 
 
@@ -7,6 +8,14 @@ var app = express();
 
 app.use(express.static(__dirname)); 
 
+var router = express.Router(); 
+
+router.post('/play', function(req, res){
+  var link = req.link; 
+  shell.exec('vlc ' + link, {async:true}); 
+}); 
+
 app.listen(3000, function(){
   console.log('listening on port', 3000); 
 }); 
+

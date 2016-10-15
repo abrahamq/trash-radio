3fd29ccd3b123ab30a1dfa6f1d66ff3bc4821186
Abraham Quintero <abrahamq@mit.edu>
2016-10-14 22:07:40 -0400

    fixed errors

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 var express = require('express'); 
 var path = require('path'); 
 var shell = require('shelljs'); 
+var bodyParser = require('body-parser'); 
 
 //var index = require('./index'); 
 
 var app = express(); 
 
 app.use(express.static(__dirname)); 
+app.use(bodyParser.urlencoded());
+
 
 var router = express.Router(); 
 
 router.post('/play', function(req, res){
-  var link = req.link; 
-  shell.exec('vlc ' + link, {async:true}); 
+  console.log("req : " + req.body); 
+  console.log("req : " + req.prototype); 
+  var link = req.body.link; 
+  //shell.exec('vlc ' + link, {async:true}); 
+  console.log(link); 
+  //shell.exec('', {async:true}); 
+  shell.exec('/Applications/VLC.app/Contents/MacOS/VLC ' + link, {async:true}); 
+  res.status(200).end(); 
 }); 
+
+app.use(router); 
 
 app.listen(3000, function(){
   console.log('listening on port', 3000); 
--- send.js
+++ send.js
@@ -1,7 +1,10 @@
 
 $( function(){
 	$( "#cmd_send" ).click(function() {
-  		var link = $("#cmd_value").val()
-  		alert( "link: "+link)
+  		var link_val = $("#cmd_value").val();
+  		$.post('./play',  { link: link_val} 
+            ).done(function(response){
+            }).fail(function(res){
+            }); 
 });
 	});

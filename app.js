744b951f8a888fa9aa9cbf2feedbc189ce2233ea
Abraham Quintero <abrahamq@mit.edu>
2016-10-14 20:04:26 -0400

    hello world 1 page app

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express'); 
+var path = require('path'); 
+
+//var index = require('./index'); 
+
+var app = express(); 
+
+app.use(express.static(__dirname)); 
+
+app.listen(3000, function(){
+  console.log('listening on port', 3000); 
+}); 
+
+//app.use('/', index); 
